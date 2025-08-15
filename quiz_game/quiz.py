22b4dc7ed801a60ddcefd89cf95501da5ff0feb6
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-08-14 23:23:26 -0600

    I made the MVP I think

--- quiz_game/quiz.py
+++ quiz_game/quiz.py
@@ -3,10 +3,10 @@
 from file_handler import write_file as write, read_file as read, intput
 import random as r
 
-def take(): # Lets the user take the quiz
+def take(): # Lets the user take the quiz by asking questions and handling answers
     qs = read()
     used_qs = []
-    counter = 0
+    counter = 1
     score = 0
     print("\n\nQuiz Topic: Geography")
 
@@ -17,18 +17,18 @@ def take(): # Lets the user take the quiz
             counter += 1
             correct = q["1"]
             print(q.pop("Question"))
-            r.shuffle(q)
-            # values_list = list(my_dict.values()) # To shuffle only values
+            # r.shuffle(q)
+            q = list(q.values())
 
-            choice = intput(f"{q["1"]}(1)\n{q["2"]}(2)\n{q["3"]}(3)\n{q["4"]}(4)\nAnswer: ", 1,4)
+            choice = intput(f"{q[0]}(1)\n{q[1]}(2)\n{q[2]}(3)\n{q[3]}(4)\nAnswer: ", 1,4)
             if choice == 1:
-                answer = q["1"]
+                answer = q[0]
             elif choice == 2:
-                answer = q["2"]
+                answer = q[1]
             elif choice == 3:
-                answer = q["3"]
+                answer = q[2]
             elif choice == 4:
-                answer = q["4"]
+                answer = q[3]
                 
             if answer == correct:
                 score += 1
