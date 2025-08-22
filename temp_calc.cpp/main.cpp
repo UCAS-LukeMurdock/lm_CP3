1641b29f5e539ac4dd3287bf562b16f3aae62954
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-08-22 12:37:02 -0600

    Finished I think

--- temp_calc.cpp/main.cpp
+++ temp_calc.cpp/main.cpp
@@ -1,2 +1,14 @@
 // LM  Fahrenheit to Celsius
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "This program converts Fahrenheit to Celsius.";
+    cout << "What is your temperature in degrees Fahrenheit?: ";
+    float num;
+    cin >> num;
+    float final_num = (num-32)*5/9;
+    cout << num << " degrees in Fahrenheit is the same as " << final_num << " degrees in Celsius.";
+    return 0;
+}
