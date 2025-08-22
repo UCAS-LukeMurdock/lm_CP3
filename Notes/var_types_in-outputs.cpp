fc4935b1af8413ca5f460e2564bf957fbd552095
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-08-22 07:27:15 -0600

    Create var_types_in-outputs.cpp

---
+++ Notes/var_types_in-outputs.cpp
@@ -0,0 +1,22 @@
+/*
+How are static and dynamically typed variables different?
+
+REMINDER: What is the difference between instantiating and declaring a variable?
+
+What case type should be used for c++ variables?
+
+What is the C++ standard library?
+
+How do you access the standard library?
+
+How do you create an output in C++?
+
+How do you create an input in C++?
+
+What is the stream in C++?
+
+What is a constant?
+
+Why do we write constants?
+
+*/
