d5c89d507016cfc2eeacc77e1a3eef0c5d084264
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-09-10 11:30:58 -0600

    finished dynamic array

--- dynamic_array/main.cpp
+++ dynamic_array/main.cpp
@@ -7,45 +7,57 @@
 using namespace std;
 
 
-int main(){
+void enter_colors(){ // This lets the user enter colors and it prints them out at the end
+    int capacity = 5;
+    shared_ptr <string[]> inputs(new string[capacity]); // Makes a shared pointer of a string array
+    int entries = 0;
 
-    cout << "\n\nWelcome to this List of Colors Program, where you can enter in as many colors as you want.\n";
-    while()
+    cout << "\n[Enter 0 to Stop]" << endl;
+    while(true){ // Loop that lets you continually enter colors into the array
+        cout << "Enter a Color: ";
+        string input;
+        getline(cin, input);
+        if(input == "0")
+            break;
 
-        int capacity = 5;
-        // auto string_array_ptr = std::make_shared<std::string[]>(array_size);
-        shared_ptr <string[]> inputs(new string[capacity]);
-        int entries = 0;
-
-        cout << "[Enter 0 to Stop]" << endl;
-        while(true){
-            cout << "Enter a Color: ";
-            string input;
-            getline(cin, input);
-            if(input == "0")
-                break;
-
-            inputs[entries] = input;
-            if(cin.fail()) break;
-            entries++;
+        inputs[entries] = input;
+        if(cin.fail()) break;
+        entries++;
 
 
-            if(entries == capacity){
-                capacity += 5;
-                shared_ptr <string[]> temp(new string[capacity]);
+        if(entries == capacity){ // Makes array dynamic
+            capacity += 5;
+            shared_ptr <string[]> temp(new string[capacity]);
 
-                for(int i = 0; i < entries; i++)
-                    temp[i] = inputs[i];
+            for(int i = 0; i < entries; i++)
+                temp[i] = inputs[i];
 
-                inputs.reset();
-                inputs = temp;
-            }
+            inputs.reset();
+            inputs = temp;
         }
+    }
+    cout << "\nEntered Colors: ";
+    for (int i=0; i < entries; i++) // Prints out the colors in the array
+        cout << inputs[i] << ", ";
+    cout << endl;
+}
 
-        for (int i=0; i < entries; i++)
-            cout << inputs[i] << ", ";
 
-    cout << "\n\n\nCome Back Soon!\n\n\n" << endl;
+int main(){ // This welcomes the user and lets the user choose to use the program or exit.
 
+    cout << "\n\nWelcome to this List of Colors Program, where you can enter in as many colors as you want.";
+    while(true){
+        cout << "\nPlay(1)  Exit(2)\nChoice: ";
+        string choice;
+        cin >> choice;
+        cin.ignore();
+
+        if(choice=="1") // Using Program
+            enter_colors();
+        else{
+            cout << "\n\n\nCome Back Soon!\n\n\n" << endl;
+            break;
+        }
+    }
     return 0;
 }
