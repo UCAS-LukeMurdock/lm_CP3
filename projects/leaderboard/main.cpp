88ce6a2c2709dd5c1405e5e328a5ccef6f9e8796
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-10-01 11:50:27 -0600

    Worked on Leaderboard

--- projects/leaderboard/main.cpp
+++ projects/leaderboard/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream> // Input and Output (i and o streams)
 #include <limits>
 #include <fstream> // read and write (if and of streams) [input and output]
-// #include <iomanip>
+#include <iomanip>
 #include <string>
 #include <vector>
 
@@ -88,6 +88,41 @@ struct Score{
 
 vector<Score> scores; // Vector to hold the scores
 
+
+int getNumber(const string& prompt){
+    int num;
+    while (true){
+        cout << prompt;
+        cin >> num;
+        if(cin.fail()){
+            cout << "\nEnter a valid number!\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the buffer
+        }else break;
+    }
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the buffer
+    return num;
+}
+
+string getString(const string& prompt){
+    string str;
+    while (true){
+        cout << prompt;
+        cin >> str;
+        cin.ignore();
+        getline(cin,str);
+        if(cin.fail()){
+            cout << "\nInvalid Input!\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the buffer
+        }else break;
+    }
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the buffer
+    return str;
+}
+
 // float num_input(){ // Checks if the input is valid (It needs to be a number [float])
 //     float input;
 //     while (!(cin >> input)){
@@ -98,92 +133,112 @@ vector<Score> scores; // Vector to hold the scores
 //     return input;
 // }
 
-void read_file(vector<Score> &scores){ // Reads the scores from the file into the vector
+// Checks inputs to make sure they don't fail
+// void check_input(){
+//     if(cin.fail()){
+//         cin.clear(); // clear error state
+//         cin.ignore(10000, '\n'); // discard invalid input completely
+//     }
+// }
+
+
+void read_file(){ // Reads the scores from the file into the vector
+    ifstream ifile;
+    ifile.open("scores.csv");
+    string line;
+
+    if(ifile.is_open()){
+        getline(ifile,line);
+
+        while(getline(ifile,line)){
+            istringstream iss(line);
+            string item;
+
+            Score temp_score;
+            getline(iss, item, ',');
+            temp_score.name = item;
+            getline(iss, item, ',');
+            temp_score.score = stoi(item);
+            getline(iss, item, ',');
+            temp_score.date = item;
+
+            scores.push_back(temp_score);
+        }
+        ifile.close();
+    }
+}
+
+void write_file(){ // Writes the scores from the vector into the file
+    ofstream ofile;
+    ofile.open("scores.csv");
+    if(ofile.is_open()){
+        for(const Score &s: scores){
+            ofile << s.name << "," << s.score << "," << s.date << endl;
+        }
+        ofile.close();
+    }
+}
+
+void sort_scores(){ // Sorts the scores in descending order
+
+    for(int i = 0; i < scores.size(); ++i){
+        for(int j = i + 1; j < scores.size(); ++j){
+            // Checks to see if the further number is greater than the closer number and if it is then it will come closer to first place.
+            if(scores[j].score > scores[i].score){
+                swap(scores[i], scores[j]);
+            }
+        }
+    }
+}
+
+void add_score(){ // Adds a new score to the vector
+
+    Score new_score;
+    cout << "\nEnter player's name: ";
+    cin >> new_score.name;
+    // -------------------------
+
+    new_score.score = getNumber("\nEnter score: ");
+
+    cout << "\nEnter date (MM/DD/YYYY): ";
+    cin >> new_score.date;
+    // -------------------------
     
-    // ifstream ifile;
-    // ifile.open("scores.txt");
-    // if(ifile.is_open()){
-    //     Score temp;
-    //     while(ifile >> temp.name >> temp.score >> temp.date){
-    //         scores.push_back(temp);
-    //     }
-    //     ifile.close();
-    // }
+    scores.push_back(new_score);
+    cout << "\nHigh Score Successfully Added!\n";
 }
 
-void write_file(const vector<Score> &scores){ // Writes the scores from the vector into the file
-    
-    // ofstream ofile;
-    // ofile.open("scores.txt");
-    // if(ofile.is_open()){
-    //     for(const Score &s: scores){
-    //         ofile << s.name << " " << s.score << " " << s.date << endl;
-    //     }
-    //     ofile.close();
-    // }
-}
+void display_scores(){ // Displays the scores in the vector
+    cout << "\nHigh Scores Leaderboard:\n";
 
-
-void sort_scores(vector<Score> &scores){ // Sorts the scores in descending order
-
-    // for(size_t i = 0; i < scores.size(); ++i){
-    //     for(size_t j = i + 1; j < scores.size(); ++j){
-    //         if(scores[j].score > scores[i].score){
-    //             swap(scores[i], scores[j]);
-    //         }
-    //     }
-    // }
-}
-
-void add_score(vector<Score> &scores){ // Adds a new score to the vector
-
-    // Score new_score;
-    // cout << "Enter player's name: ";
-    // cin >> new_score.name;
-    // cout << "Enter score: ";
-    // new_score.score = static_cast<int>(num_input());
-    // cout << "Enter date (MM/DD/YYYY): ";
-    // cin >> new_score.date;
-    // scores.push_back(new_score);
-    // cout << "\nHigh score added successfully.\n";
-}
-
-void display_scores(const vector<Score> &scores){ // Displays the scores in the vector
-    
-    // cout << "\n--- High Scores ---\n";
-    // for(size_t i = 0; i < scores.size(); ++i){
-    //     cout << i + 1 << ". Player: " << scores[i].name 
-    //         << " | Score: " << scores[i].score 
-    //         << " | Date: " << scores[i].date << endl;
-    // }
+    for(int i = 0; i < scores.size(); ++i){
+        cout << i + 1 << ". Player: " << scores[i].name 
+            << " | Score: " << scores[i].score 
+            << " | Date: " << scores[i].date << endl;
+    }
 }
 
 
 int main(){ // This welcomes the user and lets the user choose to use or exit the program.
     cout << "\n\nWelcome to this High Score Leaderboard Program, which lets you add new high scores and see the leaderboard.\n";
-    read_file(scores); // Read existing scores from file
-    display_scores(scores); // Display existing scores
+    read_file(); // Read existing scores from file
+    display_scores(); // Display existing scores
     
     while(true){
-        float choice;
-
-        cout <<
-        "\nMenu:\n"
-        "(1) Add Score\n"
-        "(2) Display Leaderboard\n"
-        "(3) Save & Exit\n"
-        "Choice: ";
-
-        choice = num_input();
+        int choice = getNumber("\nMenu:\n"
+                            "(1) Add Score\n"
+                            "(2) Display Leaderboard\n"
+                            "(3) Save & Exit\n"
+                            "Choice: ");
 
         if (choice == Add){
-            add_score(scores);
-            sort_scores(scores);
-            display_scores(scores);
+            add_score();
+            sort_scores();
+            display_scores();
         }else if(choice == Display){
-            display_scores(scores);
+            display_scores();
         }else if(choice == Exit){
-            write_file(scores);
+            write_file();
             cout << "\n\n\nCome Back Soon!\n\n\n" << endl;
             break;
         }else{
@@ -194,4 +249,5 @@ int main(){ // This welcomes the user and lets the user choose to use or exit th
 }
 
 // Fix functionality (Inputs)
+// Understand sort
 // de-Copilot it
