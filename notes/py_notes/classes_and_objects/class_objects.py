7b41f1a5a0fce6c31951e7652d8ed4635ebba3aa
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-10-10 11:41:01 -0600

    Class Notes

--- notes/py_notes/classes_and_objects/class_objects.py
+++ notes/py_notes/classes_and_objects/class_objects.py
@@ -1,36 +1,95 @@
-# LM  Classes and Objects Notes
+# LM  Classes and Objects Notes (3rd Period)
 
+class Person:
+    """Person class with first and last names as well as age.
+    Method to get full name, increase age by 1"""
+    first_name: str
+    last_name: str
+    age: int
+
+    def __init__(self, _first_name="John", last_name="Doe", age=0): # You have to add default values from right to left
+        """Sets all values"""
+        self._first_name = _first_name
+        self.last_name = last_name
+        self.age = age
+
+    # setter functions/methods change the values of the attributes
+    def setAge(self):
+        """Increase age by 1"""
+        self.age += 1
+    
+    # getter methods return/display attributes
+    def full(self):
+        """Create full name"""
+        return self._first_name + " " + self.last_name
+    
+    def __str__(self): # Kind of like overloading
+        return f"First name: {self._first_name}\nLast name: {self.last_name}\nage: {self.age}\n"
+        
+
+tia = Person("Tia", "LaRose", 28)
+alex = Person("Alex", "LaRose", 37)
+
+tia.setAge()
+
+print(f"{tia.full()}'s Age: {tia.age}")
+print(f"{alex.full()}'s Age: {alex.age}")
+
+print()
+print(tia)
 
 
 # What is a class?
-    # 
+    # A class is an abstraction of an object. It is a zoomed out version of the thing it represents. Every class has __init__ to intialize attributes for the object.
+    # It has attributes (variables) and methods (functions).
 
 # What is an object?
-    # 
+    # An instance of a class
+
+# How is a class a form of encapsulation?
+    # It puts all of the information in one place.
 
 # How is a class an abstraction of an object?
-    # 
+    # A class is an abstraction because it gets one level further from the specifics. Tia and Alex are both a person so Person is an abstraction. Person is broader so more abstract. (Covers more)
+    # It is zoomed out. It covers all/any of that type.
 
 # How do you access information in an object?
-    # 
+    # Using a getter or using object_name.info_name
 
 # How do you initialize a class?
-    # 
+    # Seen Above (either class Person or __init__)
 
 # How do you set a default value 
-    # 
+    # In the parameters of the __init__ function, set values to them from right to left.
+
+# How do you use type hinting?
+    # It double checks to make sure that the data_type stays correct.
+    # Example:
+        # first_name: str
+        # last_name: str
+        # age: int
+
+# How do you set an attribute to be private?
+    # They start public.
+    # if you add a _ before an item then it tells people that it is private and shouldn't be used outside of the function.
+
 
 # How do you make a class method?
-    # 
+    # It's just a function inside of a class. It has self in it's parameters.
 
 # Why do we include docstrings/
-    # 
+    # To document what a class is and what it is doing with multiline comments.
 
 # What does "self" do as a parameter for class methods?
-    # 
+    # It just interacts with this one object.
+    # It only does it for that current instance. (If its for Tia then it won't do it for Alex)
+
+# What are getter and setter methods?
+    # getter methods return/display attributes
+    # setter functions/methods change the values of the attributes
 
 # What are magic methods?
-    # 
+    # Special methods starting or ending with __     (def __name__)
 
 # Where are class objects saved? (heap or stack?)
-    # 
+    # Objects are saved in the Heap.
---
+++ projects/py_projects/student_class/main.py
@@ -0,0 +1,35 @@
+# LM  Student Class Project
+import csv
+# from file_handler import int_input
+
+# def int_input(prompt, min = -1, max = -1): # Checks and prompts user to solve errors in integer inputs (Has Range If Needed)
+#     try:
+#         response = int(input(prompt).strip())
+#     except:
+#         print("Not An Integer")
+#         response = int_input(prompt,min,max)
+#     if (min != -1 or max != -1) and (response < min or response > max): # If either min or max aren't -1 and the input is out of range then the user has to reinput
+#         print(f"Not In Range: {min}-{max}")
+#         response = int_input(prompt,min,max)
+#     return response
+
+def menu(): # Introduces the program and then lets the user choose one of the options
+    print("\n\nWelcome, where you can see, add, remove, or search through it.")
+    while True:
+        choice = int_input("\nDisplay(1) Search(2) Add(3) Remove(4) Edit(5) Exit(6)\n", 1,6)
+        if choice == 1:
+            ()
+        elif choice == 2:
+            ()
+        elif choice == 3:
+            ()
+        elif choice == 4:
+            ()
+        elif choice == 5:
+            ()
+        elif choice == 6:
+            print("Come Back Soon!")
+            break
+        else:
+            print("Something Broke")
+menu()
