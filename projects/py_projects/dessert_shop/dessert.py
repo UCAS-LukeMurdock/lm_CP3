9e16d32452ef6100344f0a5ef261adf279782797
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-10-28 11:48:40 -0600

    Part 1 of Part 2 of Dessert Shop

--- projects/py_projects/dessert_shop/dessert.py
+++ projects/py_projects/dessert_shop/dessert.py
@@ -1,39 +1,60 @@
 # LM  Dessert Shop Classes File
+from abc import ABC, abstractmethod
 
-class DessertItem: # Parent Class for Desserts. Has a name attribute that is shared to its children classes
-    def __init__(self, name = ""):
+class DessertItem(ABC): # Parent Class for Desserts. Has a name attribute that is shared to its children classes
+    def __init__(self, name = "", tax_percent = 7.25):
         self.name = name
+        self.tax_percent = tax_percent
+
+    @abstractmethod
+    def calculateCost(self):
+        pass
+        
+    def calculateTax(self):
+        return round(self.calculateCost() * (self.tax_percent / 100), 2)
 
 
 # Child Classes of DessertItem with extra attributes
 class Candy(DessertItem):
-    def __init__(self, name = "", price_per_pound = 0.0, candy_weight = 0.0):
-        super().__init__(name)
+    def __init__(self, name = "", price_per_pound = 0.0, candy_weight = 0.0, tax_percent = 7.25):
+        super().__init__(name, tax_percent)
         self.price_per_pound = price_per_pound
         self.candy_weight = candy_weight
 
+    def calculateCost(self):
+        return round(self.price_per_pound * self.candy_weight, 2)
+
 
 class Cookie(DessertItem):
-    def __init__(self, name = "", price_per_dozen = 0.0, cookie_amount = 0):
+    def __init__(self, name = "", price_per_dozen = 0.0, cookie_amount = 0, tax_percent = 7.25):
+        super().__init__(name, tax_percent)
         self.price_per_dozen = price_per_dozen
-        super().__init__(name)
         self.cookie_amount = cookie_amount
+
+    def calculateCost(self):
+        return round((self.price_per_dozen / 12) * self.cookie_amount, 2)
 
 
 class IceCream(DessertItem):
-    def __init__(self, name = "", price_per_scoop = 0.0, scoops = 0):
-        super().__init__(name)
+    def __init__(self, name = "", price_per_scoop = 0.0, scoops = 0, tax_percent = 7.25):
+        super().__init__(name, tax_percent)
         self.price_per_scoop = price_per_scoop
         self.scoops = scoops
 
+    def calculateCost(self):
+        return round(self.price_per_scoop * self.scoops, 2)
+
 class Sundae(IceCream): # Child Class of IceCream that has everything it has plus topping name and price.
-    def __init__(self, name = "", price_per_scoop = 0.0, scoops = 0, topping_name = "", topping_price = 0.0):
-        super().__init__(name, price_per_scoop, scoops)
+    def __init__(self, name = "", price_per_scoop = 0.0, scoops = 0, topping_name = "", topping_price = 0.0, tax_percent = 7.25):
+        super().__init__(name, tax_percent, price_per_scoop, scoops)
         self.price_per_scoop = price_per_scoop
         self.scoops = scoops
 
         self.topping_name = topping_name
         self.topping_price = topping_price
+
+    def calculateCost(self):
+        return round(super().calculateCost() + self.topping_price, 2)
 
 
 
@@ -46,4 +67,16 @@ class Order: # Order Class that has a list attribute that things can be added to
 
     def __len__(self):
         return len(self.order)
+    
+    def orderCost(self): # Calculates the total cost of the order
+        total = 0
+        for item in self.order:
+            total += item.calculateCost()
+        return round(total, 2)
+    
+    def orderTax(self): # Calculates the total tax of the order
+        total = 0
+        for item in self.order:
+            total += item.calculateTax()
+        return round(total, 2)
         
--- projects/py_projects/dessert_shop/dessert_shop.py
+++ projects/py_projects/dessert_shop/dessert_shop.py
@@ -1,21 +1,33 @@
-# LM  Dessert Shop Project
+# LM  Dessert Shop Project (Main File)
+from tabulate import tabulate
 from dessert import *
 
+# data = [["Bruce Wayne", "Batman", "123-4567"], ["Clark Kent", "Superman", "987-6543"]]
+# print(tabulate(data, headers=["Name", "Job", "Number"]))
 
 def menu(): # Creates Objects in the order and then prints them and their amount
 
     order = Order()
-    order.add(Candy("Gummy Bears", 0.34, 1.25))
+    order.add(Candy("Gummy Bears", 1.25, 0.34))
     order.add(Candy("Candy Corn", 2.45, 0.36))
-    order.add(Cookie("Chocolate Chip", 6, 2.17))
-    order.add(IceCream("Mint Chocolate Chip", 2, 1.66))
-    order.add(IceCream("Cookies and Cream", 1, 1.52))
-    order.add(Sundae("Vanilla", 2, 1.00, "Fudge", 0.33))
+    order.add(Cookie("Chocolate Chip", 2.17, 6))
+    order.add(IceCream("Mint Chocolate Chip", 1.66, 2))
+    order.add(IceCream("Cookies and Cream", 1.52, 1))
+    order.add(Sundae("Vanilla", 1.00, 2, "Fudge", 0.33))
 
     for item in order.order:
         print(item.name)
 
-    print(f"\nNumber of item in order: {len(order)}")
+    print(f"\nNumber of item in order: {len(order)}\n")
+
+    data = []
+    for item in order.order:
+        data.append([item.name, f"${item.calculateCost()}", f"${item.calculateTax()}"])
+    data.append(["-----------------", "---------", "---------"])
+    data.append(["Total", f"${round(order.orderCost(), 2)}", f"${round(order.orderTax(), 2)}"])
+    data.append(["-----------------", "---------", "---------"])
+    data.append(["Grand Total", f"${round(order.orderCost() + order.orderTax(), 2)}", ""])
+    print(tabulate(data, headers=["Item", "Cost", "Tax"]))
 
 
 menu()
